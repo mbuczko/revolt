6f0bf54b8aa6b517e0c77277bfbf2b889ed52025
Michal Buczko <michal.buczko@gmail.com>
2018-10-18 18:46:31 +0200

    sass task improved

--- src/revolt/tasks/sass.clj
+++ src/revolt/tasks/sass.clj
@@ -28,13 +28,18 @@
     ;; run SASS compilation for every single file found in
     ;; source-path, which file name does not start with _
 
-    (let [path (io/file (str (or file source-path)))]
+    (let [file (when file (io/file file))
+          partial? (and file (.startsWith (.getName file) "_"))
+          path (if partial?
+                 (.toFile source-path)
+                 (or file (.toFile source-path)))]
 
       (utils/timed
        (str "SASS " path)
        (doseq [file (file-seq path)]
          (when (and (.isFile file)
-                    (not (.startsWith (.getName file) "_")))
+                    (not (.startsWith (.getName file) "_"))
+                    (not (.endsWith (.getName file) ".min.css")))
 
            (let [relative-output (.relativize source-path (.toPath file))]
              (sass/sass-compile-to-file
